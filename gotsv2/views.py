b37e24d04822d99480af9bd3492a4e0a3258d14f
Chelsey <jacobs27c@gmail.com>
2017-10-26 20:50:32 +0100

    Add basic index view

--- gotsv2/views.py
+++ gotsv2/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Character
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the gotsv2 index.")
+    characters = Character.objects.all()
+    context = {'characters': characters}
+    return render(request, 'gotsv2/index.html', context)
