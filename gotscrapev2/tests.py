5f4f546bf1187699cae219dc364c4e1cb4d4b8f7
Chelsey <jacobs27c@gmail.com>
2017-11-01 13:00:45 +0000

    Create tests.py file for testing

---
+++ gotscrapev2/tests.py
@@ -0,0 +1,12 @@
+import requests
+from django.utils import timezone
+import pandas
+import re
+from bs4 import BeautifulSoup
+from .models import Character
+
+#does initial ASOAF test page request work
+
+#does initial ASOAF page scrape work
+
+#test for each stage of update.py method functionality
