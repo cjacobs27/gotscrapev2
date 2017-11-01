a5f80aa80b7218b276661277af399a6a40c4854a
Chelsey <jacobs27c@gmail.com>
2017-11-01 14:54:08 +0000

    Move tests.py so it is detected
    And mess about with it... no idea how to test requestNamePage method in Update

--- gotsv2/tests.py
+++ gotsv2/tests.py
@@ -1,3 +1,17 @@
+import requests
 from django.test import TestCase
+from .update import Update
+# from django.utils import timezone
+# import pandas
+# import re
+# from bs4 import BeautifulSoup
+# from .models import Character
 
-# Create your tests here.
+class UpdateMethodTests(TestCase):
+    # does initial ASOAF test page request work
+    def test_see_if_initial_request_returned(self):
+        r = Update.requestNamePage()
+
+#does initial ASOAF page scrape work
+
+#test for each stage of update.py method functionality
