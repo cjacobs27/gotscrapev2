0b261de42c35ec98549836db3822aef1d987a9a3
Chelsey <jacobs27c@gmail.com>
2017-11-02 13:23:25 +0000

    Put foriegn key in correct model
    (and made & done migrations)

--- gotsv2/models.py
+++ gotsv2/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Gender(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Character(models.Model):
     name = models.CharField(max_length=200)
     url = models.URLField(max_length=500)
@@ -8,14 +15,9 @@ class Character(models.Model):
     infobox = models.TextField()
     created_at = models.DateTimeField('created')
     updated_at = models.DateTimeField('last updated')
+    gender = models.ForeignKey(Gender)
 
     def __str__(self):
         return self.name
 
 
-class Gender(models.Model):
-    character = models.ForeignKey(Character, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.character
