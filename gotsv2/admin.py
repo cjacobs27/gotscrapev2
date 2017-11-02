c405d73e6d8844a9a6e68b3fb7dc5673f10a0efc
Chelsey <jacobs27c@gmail.com>
2017-11-02 12:29:48 +0000

    Register Gender model in admin

--- gotsv2/admin.py
+++ gotsv2/admin.py
@@ -1,6 +1,7 @@
 # Register your models here.
 from django.contrib import admin
 
-from .models import Character
+from .models import Character, Gender
 
 admin.site.register(Character)
+admin.site.register(Gender)
