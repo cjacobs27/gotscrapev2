9ee9b39c8de35e3f69e62485fcc90c9acc650847
Chelsey <jacobs27c@gmail.com>
2017-11-18 12:12:32 +0000

    Refactored encode_gender_and_update()
    title scraping methods will match this format

--- gotsv2/infoscrape.py
+++ gotsv2/infoscrape.py
@@ -1,15 +1,14 @@
 from bs4 import BeautifulSoup
-from django.db import models
 from gotsv2.models import Character, Gender
 
-class Infoscrape():
+
+class Infoscrape:
     def __init__(self):
         self.genders = []
         self.unencodedGender = []
         self.dbCharacterObjects = Character.objects.values_list('infobox', flat=True)
 
-    def encode_gender_and_update(self):
-        # scrapes infobox text, sorts into male & female, updates Character model
+    def gender_text_scrape(self):
         for item in self.dbCharacterObjects:
             html = BeautifulSoup(item, "html.parser")
             table_rows = html.find_all('tr')
@@ -21,51 +20,21 @@ class Infoscrape():
                     self.unencodedGender.append(gender)
                 else:
                     pass
-        for item in self.unencodedGender:
-            if "Female" in item:
-                self.genders.append("2")
-            else:
-                self.genders.append("1")
+
+    def label_male_and_female(self):
         a = 0
         for i in Character.objects.all():
-            update_gender = i
-            if int(self.genders[a]) == 1:
+            update_gender= i
+            if self.unencodedGender[a] == "Male":
                 update_gender.gender = Gender(1)
                 update_gender.save()
-            else:
+            elif self.unencodedGender[a] == "Female":
                 update_gender.gender = Gender(2)
                 update_gender.save()
             a = a + 1
 
-   # Commented out code is probably a better way of doing this...
-   # but not right now
-
-    # def gender_text_scrape(self):
-    #     for item in self.dbCharacterObjects:
-    #         html = BeautifulSoup(item, "html.parser")
-    #         table_rows = html.find_all('tr')
-    #         for row in table_rows:
-    #             header = row.find('th', {'scope': 'row'}, 'Gender')
-    #             value = row.find('td')
-    #             if "Gender" in str(header):
-    #                 gender = value.text
-    #                 self.unencodedGender.append(gender)
-    #             else:
-    #                 pass
-    #
-    # def label_male(self):
-    #     a = 0
-    #     for i in Character.objects.all():
-    #         update_gender= i
-    #         if self.unencodedGender[a] == "Male":
-    #             update_gender.gender = Gender(1)
-    #             update_gender.save()
-    #         elif self.unencodedGender[a] == "Female":
-    #             update_gender.gender = Gender(2)
-    #             update_gender.save()
-    #         a = a + 1
-    # def encode_gender_and_update(self):
-    #     gender_text_scrape(self)
-    #     label_male(self)
+    def encode_gender_and_update(self):
+        self.gender_text_scrape()
+        self.label_male_and_female()
 
 
