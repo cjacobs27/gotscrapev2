5bd83a6f681674c938dd5729e28fdaf8ca1af993
Chelsey <jacobs27c@gmail.com>
2018-01-03 15:09:56 +0000

    First working Infoscrape() test

--- gotsv2/tests.py
+++ gotsv2/tests.py
@@ -3,10 +3,12 @@ from django.test import TestCase
 from django.test import Client
 from .update import Update
 from .models import Character
+from .infoscrape import Infoscrape
 from datetime import datetime
 import json
 from django.utils.timezone import make_aware
 from bs4 import BeautifulSoup
+import pandas
 
 class TemplateViewTests(TestCase):
     def setUp(self):
@@ -20,11 +22,11 @@ class TemplateViewTests(TestCase):
                 one_or_two = 1
             else:
                 one_or_two = 2
-            time = datetime.now()
             clean_titles = [["title1"],["t2"],["t3"]]
             clean_titles_dict = {}
             clean_titles_dict.update({"titles": clean_titles})
             json_encoded_titles = json.JSONEncoder().encode(clean_titles_dict)
+            time = datetime.now()
             a_time = make_aware(time, timezone=None, is_dst=None)
             Character.objects.create(name='Name %s' % object_num, url='URL %s' % object_num, page = one_or_two,
                                      infobox='Infobox HTML %s' % object_num, created_at=a_time, updated_at=a_time,
@@ -63,28 +65,29 @@ class TemplateViewTests(TestCase):
 
 class InfoscrapeTests(TestCase):
     def setUp(self):
+        #client may not be needed
         self.client = Client()
-        self.genders = []
-        self.unencodedGender = []
-        self.titles = []
+        #lists for use with Infoscrape methods
+        #generating slightly different test db from before due to needing proper infobox data:
         number_of_objects = 130
+        gender_id = 1
+        test_infobox = open('gotsv2/test_infobox.html')
+        infobox_html = BeautifulSoup(test_infobox, "html.parser")
         for object_num in range(number_of_objects):
-            if object_num % 2 == 0:
-                one_or_two = 1
-            else:
-                one_or_two = 2
             time = datetime.now()
-            clean_titles = [["title1"],["t2"],["t3"]]
-            clean_titles_dict = {}
-            clean_titles_dict.update({"titles": clean_titles})
-            json_encoded_titles = json.JSONEncoder().encode(clean_titles_dict)
             a_time = make_aware(time, timezone=None, is_dst=None)
-            infobox_html = BeautifulSoup('test_infobox.html', "html.parser")
-            print(infobox_html)
-            Character.objects.create(name='Name %s' % object_num, url='URL %s' % object_num, page = one_or_two,
-                                     infobox='Infobox HTML %s' % object_num, created_at=a_time, updated_at=a_time,
-                                     gender_id= one_or_two, titles=json_encoded_titles, title_strings='title strings %s' % object_num)
-           #self.db_character_infoboxes = Character.objects.values_list('infobox', flat=True)
+            Character.objects.create(name='Name', url='URL', page = gender_id,
+                                     infobox=str(infobox_html), created_at=a_time, updated_at=a_time,
+                                     gender_id= gender_id, titles= 'titles', title_strings='title strings')
+
+    def test_gender_text_scrape_method_list_complete(self):
+        #if the unencodedGender list is 130 items (the number of fake characters in the TestCase)
+        #then gender info has been scraped from each page
+        self.db_character_infoboxes = Character.objects.values_list('infobox', flat=True)
+        i = Infoscrape()
+        output = i.gender_text_scrape()
+        self.assertEqual(len(output),130)
+
 
 # class UpdateMethodTests(TestCase):
     #all these tests might be a bit dodgy, will come back to these
