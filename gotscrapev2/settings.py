03d7bc5676ced0eb81a20ffbfc923c10bf1e3c61
Chelsey <jacobs27c@gmail.com>
2018-01-06 20:07:54 +0000

    Still not working, adding 'chelseyjacobs.herokuapp.com' to allowed_hosts

--- gotscrapev2/settings.py
+++ gotscrapev2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@3d-=b^ssb&1*-1#fqi-yaz1l%i&wl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','www.chelseyjacobs.com', 'chelseyjacobs.com']
+ALLOWED_HOSTS = ['127.0.0.1','www.chelseyjacobs.herokuapp.com','chelseyjacobs.herokuapp.com','www.chelseyjacobs.com', 'chelseyjacobs.com']
 
 
 # Application definition
