89f159b49ee9adc29ea66f24f7a3c8c903d52f45
Chelsey <jacobs27c@gmail.com>
2018-01-24 10:21:47 +0000

    Remove temporary Update view
    The worker.py automates the updates on the production server via Heroku

--- gotsv2/urls.py
+++ gotsv2/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.gotsv2, name='gotsv2'),
-    url(r'^update/', views.update, name='runupdate'),
+    # url(r'^update/', views.update, name='runupdate'),
     url(r'^graph/', views.graph, name='graph'),
     url(r'^gender-graph/',views.gender_graph_page, name='gender-graph'),
     url(r'^title-graph/',views.title_graph_page, name='title-graph'),
